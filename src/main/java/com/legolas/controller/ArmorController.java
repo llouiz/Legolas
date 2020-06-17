3fe1c3156b9c251b16d05a775eb21cfe1fd4af04
Luiz Carlos <luizcarlos--1@hotmail.com>
2020-06-17 11:46:45 -0300

    Commit

--- src/main/java/com/legolas/controller/ArmorController.java
+++ src/main/java/com/legolas/controller/ArmorController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.legolas.bean.ApiResponse;
 import com.legolas.bean.Armor;
 import com.legolas.dao.ArmorDAO;
 
@@ -29,47 +31,37 @@ public class ArmorController {
 
 	@PostMapping("/armor")
 	@PreAuthorize("hasAnyRole('ADMINISTRATOR')")
-	public Armor add(@Valid @RequestBody Armor a) {
-		return ad.save(a);
+	public ApiResponse<Armor> add(@Valid @RequestBody Armor a) {
+		return new ApiResponse<>(HttpStatus.OK.value(), "Armor saved successfully.", ad.save(a));
 	}
 
 	@GetMapping("/armor")
-	public List<Armor> getAllArmor() {
-		return ad.findAll();
+	public ApiResponse<List<Armor>> getAllArmor() {
+		return new ApiResponse<>(HttpStatus.OK.value(), "Armor list fetched successfully.", ad.findAll());
 
 	}
 
 	@GetMapping("/armor/{id}")
-	public ResponseEntity<Armor> getArmorById(@PathVariable(value = "id") Long id) {
-		Armor a = ad.findOne(id);
-
-		if (id == null) {
-			return ResponseEntity.notFound().build();
-		}
-
-		return ResponseEntity.ok().body(a);
+	public ApiResponse<Armor> getArmorById(@PathVariable(value = "id") Long id) {
+		return new ApiResponse<>(HttpStatus.OK.value(), "Armor fetched successfully.", ad.findOne(id));
 	}
 
 	@PutMapping("/armor/{id}")
 	@PreAuthorize("hasAnyRole('ADMINISTRATOR')")
-	public ResponseEntity<Armor> updateArmor(@PathVariable(value = "id") Long id, @Valid @RequestBody Armor a) {
+	public ApiResponse<Armor> updateArmor(@PathVariable(value = "id") Long id, @Valid @RequestBody Armor a) {
+		Armor r = ad.findOne(a.getId());
 
-		Armor r = ad.findOne(id);
-
-		if (r == null) {
-			return ResponseEntity.notFound().build();
+		if (r != null) {
+			BeanUtils.copyProperties(a, r, "id");
+			r = ad.save(r);
 		}
 
-		BeanUtils.copyProperties(a, r, "id");
-
-		r = ad.save(r);
-
-		return ResponseEntity.ok(r);
+		return new ApiResponse<>(HttpStatus.OK.value(), "Armor updated successfully.", r);
 	}
 
 	@DeleteMapping("/armor/{id}")
 	@PreAuthorize("hasAnyRole('ADMINISTRATOR')")
-	public ResponseEntity<Armor> delete(@PathVariable(value = "id") Long id) {
+	public ApiResponse<Void> delete(@PathVariable(value = "id") Long id) {
 		Armor a = ad.findOne(id);
 
 		if (a == null) {
@@ -78,6 +70,15 @@ public class ArmorController {
 
 		ad.delete(a);
 
-		return ResponseEntity.ok().build();
+		return new ApiResponse<>(HttpStatus.OK.value(), "Armor deleted successfully.", null);
+	}
+
+	@DeleteMapping("/armor/all")
+	@PreAuthorize("hasAnyRole('ADMINISTRATOR')")
+	public ApiResponse<Void> deleteAll() {
+
+		ad.deleteAll();
+
+		return new ApiResponse<>(HttpStatus.OK.value(), "Weapon deleted successfully.", null);
 	}
 }
