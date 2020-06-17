604f7ed297769d09e8b3b733be5c37b807d1275d
Luiz Carlos <luizcarlos--1@hotmail.com>
2020-06-17 11:02:42 -0300

    Second commit

--- src/main/java/com/legolas/dao/PlayerDAO.java
+++ src/main/java/com/legolas/dao/PlayerDAO.java
@@ -3,6 +3,7 @@ package com.legolas.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.legolas.bean.Players;
@@ -10,28 +11,32 @@ import com.legolas.repository.PlayerRepository;
 
 @Service
 public class PlayerDAO {
-	
+
 	@Autowired
 	PlayerRepository rep;
-	
+
 	public Players save(Players c) {
 		return rep.save(c);
-		
+
 	}
+
 	public List<Players> findAll() {
 		return rep.findAll();
 	}
-	
+
 	public Players findOne(Long obj_id) {
 		return rep.findById(obj_id).get();
 	}
-	
+
 	public void delete(Players c) {
 		rep.delete(c);
 	}
-	
+
 	public void deleteAll() {
 		rep.deleteAll();
 	}
 
+	public List<Players> getAllTopKills(Sort topKill) {
+		return rep.findAll(topKill); 
+	}
 }
